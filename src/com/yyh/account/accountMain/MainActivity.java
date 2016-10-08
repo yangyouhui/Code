081802ff84ec157cb8a6babc514f1771b82ffd42
ios01 <ios01@localhost>
2016-10-08 14:30:43 -0700

    添加了主页按钮链接

--- src/com/yyh/account/accountMain/MainActivity.java
+++ src/com/yyh/account/accountMain/MainActivity.java
@@ -3,15 +3,38 @@ package com.yyh.account.accountMain;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.yyh.account.R;
-import com.yyh.account.inaccount.InaccountActivity;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void InaccountClick(View v){
+    	
+    	Intent intent =new Intent();
+    	intent.setClass(MainActivity.this,com.yyh.account.inaccount.InaccountActivity.class);
+    	startActivity(intent);
+    	
+    }
+    
+    public void OutaccountClick(View v){
+    	
+    	Intent intent =new Intent();
+    	intent.setClass(MainActivity.this,com.yyh.account.accountout.OutaccountActivity.class);
+    	startActivity(intent);
+    	
+    }
+    
+    public void FlagClick(View v){
+    	
+    	Intent intent =new Intent();
+    	intent.setClass(MainActivity.this,com.yyh.account.flag.FlagActivity.class);
+    	startActivity(intent);
+    	
     }
 }
