30aaca29d0fe608f2a30bf986c2b765cf66b4b23
ios22 <ios22@localhost>
2016-10-08 10:52:50 +0800

    yq1

--- gen/com/yyh/account/R.java
+++ gen/com/yyh/account/R.java
@@ -11,15 +11,36 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int inaccount=0x7f020002;
+        public static final int background=0x7f020000;
+        public static final int backtc=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int bianq=0x7f020003;
+        public static final int bottomback=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int inaccount=0x7f020006;
+        public static final int shez=0x7f020007;
+        public static final int shour=0x7f020008;
+        public static final int tub=0x7f020009;
+        public static final int tuic=0x7f02000a;
+        public static final int zhic=0x7f02000b;
+        public static final int zjbianq=0x7f02000c;
     }
     public static final class id {
+        public static final int imageButt=0x7f060001;
+        public static final int imageButton1=0x7f060002;
+        public static final int imageButton2=0x7f060004;
+        public static final int imageButton3=0x7f060006;
+        public static final int imageButton4=0x7f060008;
+        public static final int imageButton5=0x7f06000a;
         public static final int lvshow=0x7f060000;
-        public static final int tvhandler=0x7f060002;
-        public static final int tvmoney=0x7f060001;
-        public static final int tvtime=0x7f060003;
+        public static final int tvhandler=0x7f06000d;
+        public static final int tvmoney=0x7f06000c;
+        public static final int tvtime=0x7f06000e;
+        public static final int txtVBianQ=0x7f060007;
+        public static final int txtVSheZ=0x7f060009;
+        public static final int txtVShouR=0x7f060005;
+        public static final int txtVTuiC=0x7f06000b;
+        public static final int txtVZHC=0x7f060003;
     }
     public static final class layout {
         public static final int activity_addflag=0x7f030000;
@@ -38,8 +59,27 @@ public final class R {
         public static final int item=0x7f03000d;
     }
     public static final class string {
+        public static final int Biaot=0x7f040008;
+        public static final int Times=0x7f04000b;
+        public static final int ZuoZ=0x7f04000a;
+        public static final int action_settings=0x7f040002;
+        /**   主页面 
+         */
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int titlef=0x7f040003;
+        public static final int txtAdd=0x7f04000e;
+        public static final int txtAddtime=0x7f040010;
+        public static final int txtBianq=0x7f040009;
+        public static final int txtContentTitle=0x7f04000f;
+        /**  便签管理页面 
+         */
+        public static final int txtGuanl=0x7f04000d;
+        public static final int txtShouR=0x7f040005;
+        public static final int txtTuiC=0x7f04000c;
+        public static final int txtZhiC=0x7f040004;
+        public static final int txtbianq=0x7f040006;
+        public static final int txtshez=0x7f040007;
     }
     public static final class style {
         /** 
@@ -68,5 +108,11 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int Imagestyle=0x7f050004;
+        public static final int LinearLayoutTwo=0x7f050003;
+        /**  主页面布局 
+         */
+        public static final int LinearLayoutone=0x7f050002;
+        public static final int WenB=0x7f050005;
     }
 }
