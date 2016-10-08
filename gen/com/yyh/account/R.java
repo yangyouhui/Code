7ab84c5fc0fe5b0919d95293223361222d30d3cf
ios01 <ios01@localhost>
2016-10-08 14:41:39 -0700

    支出管理页面布局

--- gen/com/yyh/account/R.java
+++ gen/com/yyh/account/R.java
@@ -18,11 +18,11 @@ public final class R {
         public static final int bottomback=0x7f020004;
         public static final int ic_launcher=0x7f020005;
         public static final int inaccount=0x7f020006;
-        public static final int shez=0x7f020007;
-        public static final int shour=0x7f020008;
-        public static final int tub=0x7f020009;
-        public static final int tuic=0x7f02000a;
-        public static final int zhic=0x7f02000b;
+        public static final int outaccount=0x7f020007;
+        public static final int shez=0x7f020008;
+        public static final int shour=0x7f020009;
+        public static final int tub=0x7f02000a;
+        public static final int tuic=0x7f02000b;
         public static final int zjbianq=0x7f02000c;
     }
     public static final class id {
